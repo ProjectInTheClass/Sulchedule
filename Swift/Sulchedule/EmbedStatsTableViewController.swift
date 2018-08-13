14cb7924146930d7ae270aa2f008cb1a43d56d99
Junoh Cho <herojeff02@gmail.com>
2018-08-14 02:00:56 +0900

    [UI] last 14 days table is now arranged
    shame I used bubble sort...

--- Swift/Sulchedule/EmbedStatsTableViewController.swift
+++ Swift/Sulchedule/EmbedStatsTableViewController.swift
@@ -13,19 +13,120 @@ class EmbedStatsTableViewController: UITableViewController {
             tableTitles.append("총 열량")
             tableValues.append("\(getWeeklyExpense())원")
             tableValues.append("\(getWeeklyCalorie())kcal")
-            for item in getWeeklySul() {
-                tableTitles.append(sul[item.key].displayName)
-                tableValues.append("\(item.value)\(getSulUnit(index: item.key))")
-                //Add Unit!
+            
+            if(getWeeklySul().count > 1){
+                var cnt = 0
+                var i = -1
+                let currentDictionary = getWeeklySul()
+                var valueArr:[Int] = []
+                var indexArr:[Int] = []
+                while(cnt < currentDictionary.count){
+                    i += 1
+                    if(currentDictionary[i] != nil){
+                        valueArr.append(currentDictionary[i]!)
+                        indexArr.append(i)
+                        cnt += 1
+                    }
+                }
+                let count = valueArr.count - 1
+                for i in 0...count{
+                    for j in 0...count{
+                        if(valueArr[i]>valueArr[j]){
+                            let temp = valueArr[i]
+                            valueArr[i] = valueArr[j]
+                            valueArr[j] = temp
+                            let temp2 = indexArr[i]
+                            indexArr[i] = indexArr[j]
+                            indexArr[j] = temp2
+                        }
+                    }
+                }
+                
+                for i in 0...count {
+                    tableTitles.append(sul[indexArr[i]].displayName)
+                    tableValues.append("\(valueArr[i])\(getSulUnit(index: indexArr[i]))")
+                }
             }
-            for item in getWeeklyFriend() {
-                tableTitles.append(item.key)
-                tableValues.append("\(item.value)회 합석")
+            else{
+                for item in getWeeklySul() {
+                    tableTitles.append(sul[item.key].displayName)
+                    tableValues.append("\(item.value)\(getSulUnit(index: item.key))")
+                }
             }
-            for item in getWeeklyLocation() {
-                tableTitles.append(item.key)
-                tableValues.append("\(item.value)회 방문")
+            
+            if(getWeeklyFriend().count > 1){
+                var cnt = 0
+                var i = -1
+                let currentDictionary = getWeeklyFriend()
+                var valueArr:[Int] = []
+                var nameArr:[String] = []
+                for item in currentDictionary{
+                    nameArr.append(item.key)
+                    valueArr.append(item.value)
+                }
+                let count = valueArr.count - 1
+                for i in 0...count{
+                    for j in 0...count{
+                        if(valueArr[i]>valueArr[j]){
+                            let temp = valueArr[i]
+                            valueArr[i] = valueArr[j]
+                            valueArr[j] = temp
+                            let temp2 = nameArr[i]
+                            nameArr[i] = nameArr[j]
+                            nameArr[j] = temp2
+                        }
+                    }
+                }
+                
+                for i in 0...count {
+                    tableTitles.append(nameArr[i])
+                    tableValues.append("\(valueArr[i])회 합석")
+                }
             }
+            else{
+                for item in getWeeklyFriend() {
+                    tableTitles.append(item.key)
+                    tableValues.append("\(item.value)회 합석")
+                }
+            }
+
+            if(getWeeklyLocation().count > 1){
+                var cnt = 0
+                var i = -1
+                let currentDictionary = getWeeklyLocation()
+                var valueArr:[Int] = []
+                var locArr:[String] = []
+                for item in currentDictionary{
+                    locArr.append(item.key)
+                    valueArr.append(item.value)
+                }
+                let count = valueArr.count - 1
+                for i in 0...count{
+                    for j in 0...count{
+                        if(valueArr[i]>valueArr[j]){
+                            let temp = valueArr[i]
+                            valueArr[i] = valueArr[j]
+                            valueArr[j] = temp
+                            let temp2 = locArr[i]
+                            locArr[i] = locArr[j]
+                            locArr[j] = temp2
+                        }
+                    }
+                }
+                
+                for i in 0...count {
+                    tableTitles.append(locArr[i])
+                    tableValues.append("\(valueArr[i])회 방문")
+                }
+            }
+            else{
+                for item in getWeeklyLocation() {
+                    tableTitles.append(item.key)
+                    tableValues.append("\(item.value)회 방문")
+                }
+            }
+            
+
         }
         else{
             tableTitles.append("총 지출액")
